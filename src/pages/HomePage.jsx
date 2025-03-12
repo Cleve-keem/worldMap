b90795ce73da26cd3d1b97a377562d39d71cab4d
Hackhim <belloharyourmidey@gmail.com>
2025-03-12 10:09:21 -0700

    added text to the home page and the start tracking now button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import PageNavbar from "../Components/PageNavbar";
+import styles from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
     <>
       <PageNavbar />
       <div className={styles.homePage}>
-        <h2>
+        <h1>
           As you travel the world. WorldMap keeps track of your adventures.
-        </h2>
+        </h1>
         <p>
-          The world map that tracks your footsteps into every city you can think of. Never forget your wonderful experiences, and show your friends how you have wandered the world.
+          The world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
         </p>
+        <button>START TRACKING NOW</button>
       </div>
     </>
   );
