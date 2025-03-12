ef9c5d1f69bfc8a5d0905973e20410dbc5d06764
Hackhim <belloharyourmidey@gmail.com>
2025-03-12 02:11:17 -0700

    created the pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <div>App component</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
