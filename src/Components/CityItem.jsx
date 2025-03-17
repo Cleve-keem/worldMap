595eb200d3dbe28080603d64e293b7636de3b394
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-17 09:40:29 -0700

    added message component and css module

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./CityItem.module.css";
 
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 export default function CityItem({ city }) {
   const { cityName, emoji, date } = city;
   return (
     <li className={styles.cityItem}>
       <span className={styles.cityEmoji}>{emoji}</span>
       <h3 className={styles.cityName}>{cityName}</h3>
-      <time className={styles.cityDate}>{date}</time>
+      <time className={styles.cityDate}>{formatDate(date)}</time>
       <button className={styles.cityDelBtn}>&times;</button>
     </li>
   );
