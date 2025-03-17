14d31300b0a602e56d37b17eb7d2c791bd96eb54
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-17 10:00:54 -0700

    modified the message css and cityList

--- src/Components/CityList.jsx
+++ src/Components/CityList.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
-
+import Message from "./Message";
 
 export default function CityList({ cities, isLoading }) {
   if (isLoading) {
     return <p>LOADING...</p>;
   }
+
+  if (!cities.length) {
+    return <Message message="Add your first city by clicking on a city on the map" />;
+  }
   return (
     <div className={styles.cityList}>
       <ul>
         {cities.map((city) => (
-          <CityItem city={city} key={city.id}/>
+          <CityItem city={city} key={city.id} />
         ))}
       </ul>
     </div>
