205bff9d923c8695a9c9541c3a289fd0b17071b1
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-29 13:51:28 -0700

    added form css module

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import styles from "./Form.module.css";
 
 export default function Form() {
-  return <form>
-    <div className={style.row}>
+  return (
+    <form className={styles.form}>
+      <div className={styles.row}>
         <label htmlFor="cityName">City Name</label>
         <input type="text" />
-    </div>
-  </form>;
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="cityName">When did you go to?</label>
+        <input type="text" />
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="cityName">Notes about your trip to</label>
+        <textarea />
+      </div>
+
+      <div className={styles.buttons}>
+        <button>Add</button>
+        <button>&larr; Back</button>
+      </div>
+    </form>
+  );
 }
