e991a1bd56d34dd7fa994c48a614de8a72628a88
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-29 14:27:13 -0700

    added-navigator-to-form

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+
   return (
-    <div className={styles.map}>
+    <div
+      className={styles.map}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <p>Map</p>
       <p>
         Position: latitude={lat}, langitude={lng}
