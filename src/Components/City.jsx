2f60badfc580235f0e71a7e1af188cc48639cfb3
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-30 08:17:03 -0700

    added a correct json format

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./City.module.css";
 import { useParams } from "react-router-dom";
+import { useCities } from "../Contexts/CitiesContext";
 
 export default function City() {
   const { id } = useParams();
-  
+  const { currentCity, getCity } = useCities();
+
   return <p>City</p>;
 }
--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -1,19 +1,19 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const BASE_URL = "http://localhost:5173"; // Base url
+const BASE_URL = "http://localhost:5000"; // Base url
 
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
       try {
         setIsLoading(true);
         const res = await fetch(`${BASE_URL}/data/cities.json`);
-
         if (!res.ok) {
           throw new Error(`HTTP error! Status: ${res.status}`);
         }
@@ -28,11 +28,30 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/data/cities.json/${id}`);
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        getCity,
+        currentCity,
       }}
     >
       {children}
@@ -42,7 +61,6 @@ function CitiesProvider({ children }) {
 
 function useCities() {
   const context = useContext(CitiesContext);
-  console.log(context);
   return context;
 }
 
