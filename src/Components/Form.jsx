ee0e94d1964865fed44f107cc92bfe186d80d189
Cleve-keem <belloharyourmidey@gmail.com>
2025-04-01 04:30:46 -0700

    adding the necessary useState in the form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../Hooks/useUrlPosition";
+import { use } from "react";
 
 export default function Form() {
+  const [cityName, setCityName] = useState("");
+  const [countryName, setCountry] = useState("");
+  const [date, setDate] = useState(new Date());
+
+  // const { lat, lng } = useUrlPosition();
+
+  useEffect(function () {}, []);
+
   return (
     <form className={styles.form}>
       <div className={styles.row}>
         <label htmlFor="cityName">City Name</label>
-        <input type="text" />
+        <input type="text" value={cityName} />
+
+        {/* <span>emoji</span> */}
       </div>
 
       <div className={styles.row}>
         <label htmlFor="cityName">When did you go to?</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
       </div>
 
       <div className={styles.row}>
